0769f4feb815e4be4c57044da630b2b8058b495e
Alex Thalhammer <alex@angular-software.at>
2021-09-13 08:02:42 +0200

    [NgRx] Selectors Bonus: Using feature selectors

--- apps/flight-app/src/app/flight-booking/+state/flight-booking.selectors.ts
+++ apps/flight-app/src/app/flight-booking/+state/flight-booking.selectors.ts
@@ -1,13 +1,10 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 import * as fromFlightBooking from './flight-booking.reducer';
-import { FlightBookingAppState, flightBookingFeatureKey } from './flight-booking.reducer';
 
-/*export const selectFlightBookingState = createFeatureSelector<fromFlightBooking.State>(
-  fromFlightBooking.flightBookingFeatureKey
-);*/
+export const selectFlightBookingState = createFeatureSelector<fromFlightBooking.State>(fromFlightBooking.flightBookingFeatureKey);
 
-export const selectFlights = (appState: FlightBookingAppState) => appState[flightBookingFeatureKey].flights;
-export const negativeList = (appState: FlightBookingAppState) => appState[flightBookingFeatureKey].negativeList;
+export const selectFlights = createSelector(selectFlightBookingState, (featureState) => featureState.flights);
+export const negativeList = createSelector(selectFlightBookingState, (featureState) => featureState.negativeList);
 
 export const selectFilteredFlights = createSelector(selectFlights, negativeList, (flights, negativeList) =>
   flights.filter((f) => !negativeList.includes(f.id))
