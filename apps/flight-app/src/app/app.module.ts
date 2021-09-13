47904184e357da0f88ac4cf270f1ae86af531266
Alex Thalhammer <alex@angular-software.at>
2021-09-13 07:51:08 +0200

    [NgRx] Store: Setup the Store for the App- and the Feature Module

--- apps/flight-app/src/app/app.module.ts
+++ apps/flight-app/src/app/app.module.ts
@@ -18,6 +18,11 @@ import { CustomLogFormatterService } from './shared/logging/custom-log-formatter
 
 import { ReactiveFormsModule } from '@angular/forms';
 import { FlightLookaheadComponent } from './flight-lookahead/flight-lookahead.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './+state';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   imports: [
@@ -34,7 +39,11 @@ import { FlightLookaheadComponent } from './flight-lookahead/flight-lookahead.co
 
     LoggerModule.forRoot({ enableDebug: true, logFormatterType: CustomLogFormatterService }),
 
-    ReactiveFormsModule
+    ReactiveFormsModule,
+
+    StoreModule.forRoot(reducers, { metaReducers }),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    EffectsModule.forRoot([])
   ],
   declarations: [AppComponent, SidebarComponent, NavbarComponent, HomeComponent, BasketComponent, FlightLookaheadComponent],
   providers: [],
