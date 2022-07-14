fee64afdc3f98d69d5e722ccce02735a51e8e9ba
Alex Thalhammer <alex@angular-software.at>
2022-07-14 10:13:20 +0200

    [RxJS] Bonus: Add delays and show a loading state

--- apps/flight-app/src/app/flight-booking/airport/airport.component.ts
+++ apps/flight-app/src/app/flight-booking/airport/airport.component.ts
@@ -2,7 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AirportService } from '@flight-workspace/flight-lib';
 import { Observable, Observer, Subject, Subscription } from 'rxjs';
 import { HttpErrorResponse } from '@angular/common/http';
-import { share, takeUntil } from 'rxjs/operators';
+import { delay, share, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'flight-workspace-airport',
@@ -14,18 +14,20 @@ export class AirportComponent implements OnInit, OnDestroy {
 
   // 1 using subscription & unsubscribe
   airports: string[] = [];
+  airportsIsLoading = true;
   private airportsObserver: Observer<string[]> | undefined;
   private airportsSubscription: Subscription | undefined;
 
   // 2 takeUntil & subject
   airportsTakeUntil: string[] = [];
+  airportsTakeUntilIsLoading = true;
   private terminatorSubject = new Subject<void>();
   readonly terminator$ = this.terminatorSubject.asObservable();
 
   constructor(private airportService: AirportService) {}
 
   ngOnInit(): void {
-    this.airports$ = this.airportService.findAll().pipe(share());
+    this.airports$ = this.airportService.findAll().pipe(delay(3000), share());
 
     this.airportsObserver = {
       next: (airports) => this.onLoadAirportsSuccessfully(airports),
@@ -43,9 +45,11 @@ export class AirportComponent implements OnInit, OnDestroy {
       next: (airports) => {
         console.log('airportsTakeUntil$ next: ', airports);
         this.airportsTakeUntil = airports;
+        this.airportsTakeUntilIsLoading = false;
       },
       error: (err) => {
         console.error('airportsTakeUntil$ error: ', err);
+        this.airportsTakeUntilIsLoading = false;
       },
       complete: () => {
         console.warn('airportsTakeUntil$ completed');
@@ -65,9 +69,11 @@ export class AirportComponent implements OnInit, OnDestroy {
   private onLoadAirportsSuccessfully(airports: string[]): void {
     console.log('airports$ next: ' + airports);
     this.airports = airports;
+    this.airportsIsLoading = false;
   }
 
   private onLoadAirportsFail(err: HttpErrorResponse): void {
     console.error('airports$ error: ' + err);
+    this.airportsIsLoading = false;
   }
 }
