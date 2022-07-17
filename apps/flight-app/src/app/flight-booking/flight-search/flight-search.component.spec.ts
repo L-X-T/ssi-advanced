9e30b80527cbfa1d86ba88160106592261ad0b84
Alex Thalhammer <alex@angular-software.at>
2022-07-17 21:35:51 +0200

    [unit] Bonus: Unit-Test with Mocking

--- apps/flight-app/src/app/flight-booking/flight-search/flight-search.component.spec.ts
+++ apps/flight-app/src/app/flight-booking/flight-search/flight-search.component.spec.ts
@@ -1,21 +1,67 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 
 import { FlightSearchComponent } from './flight-search.component';
-import { FlightBookingModule } from '../flight-booking.module';
-import { SharedModule } from '../../shared/shared.module';
 
-import { FlightService } from '@flight-workspace/flight-lib';
+import { Flight, FlightService } from '@flight-workspace/flight-lib';
+import { Observable, of } from 'rxjs';
+import { Component, Directive, EventEmitter, Input, Output, Pipe, PipeTransform } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 describe('Unit test: flight-search.component', () => {
   let component: FlightSearchComponent;
   let fixture: ComponentFixture<FlightSearchComponent>;
 
+  const resultMock = [
+    { id: 17, from: 'Graz', to: 'Hamburg', date: 'now', delayed: true },
+    { id: 18, from: 'Vienna', to: 'Barcelona', date: 'now', delayed: true },
+    { id: 19, from: 'Frankfurt', to: 'Salzburg', date: 'now', delayed: true }
+  ];
+
+  const flightServiceMock = {
+    find(from: string, to: string): Observable<Flight[]> {
+      return of(resultMock);
+    },
+    // Implement the following members only if this code is used in your Component
+    flights: [] as Flight[],
+    load(from: string, to: string): void {
+      this.find(from, to).subscribe((f) => {
+        this.flights = f;
+      });
+    }
+  };
+
+  @Component({ selector: 'flight-card', template: '' })
+  class FlightCardComponent {
+    @Input() item: Flight | undefined;
+    @Input() selected = false;
+    @Output() selectedChange = new EventEmitter<boolean>();
+  }
+
+  // eslint-disable-next-line @angular-eslint/directive-selector
+  @Directive({ selector: 'input[city]' })
+  class CityValidatorDirective {
+    @Input() city: string[] = [];
+    validate = (_: any) => null;
+  }
+
+  @Pipe({ name: 'city' })
+  class CityPipe implements PipeTransform {
+    transform = (v: string) => v;
+  }
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule, RouterTestingModule, FlightBookingModule, SharedModule],
-      providers: [FlightService]
+      imports: [FormsModule],
+      declarations: [FlightSearchComponent, FlightCardComponent, CityPipe, CityValidatorDirective]
+    }).overrideComponent(FlightSearchComponent, {
+      set: {
+        providers: [
+          {
+            provide: FlightService,
+            useValue: flightServiceMock
+          }
+        ]
+      }
     });
 
     fixture = TestBed.createComponent(FlightSearchComponent);
@@ -27,7 +73,7 @@ describe('Unit test: flight-search.component', () => {
     expect(component.flights.length).toBe(0);
   });
 
-  it('should load flights when user entered from and to', () => {
+  /*it('should load flights when user entered from and to', () => {
     component.from = 'Graz';
     component.to = 'Hamburg';
     component.search();
@@ -46,5 +92,21 @@ describe('Unit test: flight-search.component', () => {
     ]);
 
     expect(component.flights.length).toBe(1);
+  });*/
+
+  it('should not load flights w/o from and to', () => {
+    component.from = '';
+    component.to = '';
+    component.search();
+
+    expect(component.flights.length).toBe(0);
+  });
+
+  it('should load flights w/ from and to', () => {
+    component.from = 'Hamburg';
+    component.to = 'Graz';
+    component.search();
+
+    expect(component.flights.length).toBeGreaterThan(0);
   });
 });
