f10b5fcefc87b19b4b23f1ae4567ad835e74c280
Alex Thalhammer <alex@angular-software.at>
2022-07-18 11:23:53 +0200

    [M-F] Bonus: Switch to Dynamic Federation

--- apps/flight-app/src/app/app.routes.ts
+++ apps/flight-app/src/app/app.routes.ts
@@ -1,6 +1,8 @@
-import { Route, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { BasketComponent } from './basket/basket.component';
 import { HomeComponent } from './home/home.component';
+
+import { loadRemoteModule } from '@angular-architects/module-federation';
 
 export const APP_ROUTES: Routes = [
   {
@@ -19,8 +21,13 @@ export const APP_ROUTES: Routes = [
   },
   {
     path: 'mf-passenger',
-    loadChildren: () => import('passenger/module').then((esm) => esm.PassengerModule)
-  } as Route,
+    loadChildren: () =>
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: 'http://localhost:4201/remoteEntry.js',
+        exposedModule: './module'
+      }).then((esm) => esm.PassengerModule)
+  },
   {
     path: '**',
     redirectTo: 'home'
