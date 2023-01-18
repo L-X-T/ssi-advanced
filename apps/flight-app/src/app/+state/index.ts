77fa4080590854e1a21c30bb1007a5fc88f5e5c6
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2023-01-18 15:49:59 +0100

    [NgRx] Add persisting of local storage
    (cherry picked from commit e20aa9524243b663a0f0b4a7b599832a0a99ac7c)
    (cherry picked from commit 0ce7a6043be7ed3b8929fd3be549d72d1fae9381)

--- apps/flight-app/src/app/+state/index.ts
+++ apps/flight-app/src/app/+state/index.ts
@@ -1,6 +1,8 @@
-import { ActionReducerMap, MetaReducer } from '@ngrx/store';
+import { ActionReducer, ActionReducerMap, MetaReducer } from '@ngrx/store';
 import { routerReducer } from '@ngrx/router-store';
 import { environment } from '../../environments/environment';
+import { localStorageSync } from 'ngrx-store-localstorage';
+import { flightBookingFeatureKey } from '../flight-booking/+state/flight-booking.reducer';
 
 export interface State {}
 
@@ -8,4 +10,8 @@ export const reducers: ActionReducerMap<State> = {
   router: routerReducer
 };
 
-export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
+export function localStorageSyncReducer(reducer: ActionReducer<any>): ActionReducer<any> {
+  return localStorageSync({ keys: [flightBookingFeatureKey], rehydrate: true })(reducer);
+}
+
+export const metaReducers: Array<MetaReducer<any, any>> = [localStorageSyncReducer];
